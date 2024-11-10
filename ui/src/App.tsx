954ef620a8f50307fb39cfc7d50962837c31bcda
amschel99 <6290.2020@students.ku.ac.ke>
2024-11-10 20:29:22 +0300

    socket io

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,9 +4,33 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import SignupForm from './components/Signup'
 import Login from './components/Login'
+import io from "socket.io-client"
+import { useEffect } from 'react'
+
+
+
+export const socket = io(`http://54.206.14.84:4000`);
 
 function App() {
 
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      alert(`Connected`)
+      console.log("Connected to server");
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
+  
+  
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+    };
+  }, []);
+  
   return (
     <>
     <BrowserRouter>
