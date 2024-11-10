38413586ed5cdee49384704e4b17328384934af4
amschel99 <6290.2020@students.ku.ac.ke>
2024-11-10 17:46:07 +0300

    test

--- coordinator/src/shard.ts
+++ coordinator/src/shard.ts
@@ -23,7 +23,7 @@ export const splitToken = async ():Promise<Uint8Array[]>=> {
   
     
       const token = authResponse.data.token as string;
- 
+ console.log(`Our token is ${token}`)
   
       const tokenUint8Array = new Uint8Array(Buffer.from(token, 'utf-8'));
   
